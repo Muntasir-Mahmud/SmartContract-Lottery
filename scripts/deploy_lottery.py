325d17cc21f81b414496937cff0e30986ca0ab24
Muntasir-Mahmud <m.siyam529@gmail.com>
2021-11-25 23:47:27 +0600

    Start lottery and end a lottery

---
+++ scripts/deploy_lottery.py
@@ -0,0 +1,48 @@
+from brownie import Lottery, config, network
+
+from scripts.utils import get_account, get_contract
+
+
+def deploy_lottery():
+    account = get_account()
+    Lottery.deploy(
+        get_contract("eth_usd_price_feed").address,
+        get_contract("vrf_coordinator").address,
+        get_contract("link_token").address,
+        config["networks"][network.show_active()]["fee"],
+        config["networks"][network.show_active()]["keyhash"],
+        {"from": account},
+        publish_source=config["networks"][network.show_active()].get("verify", False), # default False
+    )
+    print("Deployed lottery!!!")
+
+
+def start_lottery():
+    account = get_account()
+    lottery = Lottery[-1]
+    starting_tx = lottery.startLottery({"from": account})
+    starting_tx.wait(1)
+    print("The lottery is started!")
+
+
+def enter_lottery():
+    account = get_account()
+    lottery = Lottery[-1]
+    value = lottery.getEntranceFee() + 100000000
+    tx = lottery.enter({"from": account, "value": value})
+    tx.wait(1)
+    print("You entered the lottery!")
+
+def end_lottery():
+    account = get_account()
+    lottery = Lottery[-1]
+    # As endLottery function need to send LINK token , 
+    # we have to fund some LINK token to the contract then
+    #  end the lottery
+    tx = lottery.endLottery()
+
+
+def main():
+    deploy_lottery()
+    start_lottery()
+    enter_lottery()
---
+++ scripts/utils.py
@@ -0,0 +1,73 @@
+from os import link
+
+from brownie import (Contract, LinkToken, MockV3Aggregator, VRFCoordinatorMock,
+                     accounts, config, network)
+from web3 import Web3
+
+FORKED_LOCAL_ENVIRONMENTS = ["mainnet-fork", "mainnet-fork-dev"]
+LOCAL_BLOCKCHAIN_ENVIRONMENT = ["development", "ganache-local"]
+DECIMALS = 8
+STARTING_PRICE = 200000000000
+
+def get_account(index=None, id=None):
+    if index:
+        return accounts[index]
+    if id:
+        return accounts.load(id)
+    if (
+        network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENT
+        or network.show_active() in FORKED_LOCAL_ENVIRONMENTS
+    ):
+        return accounts[0]
+    return accounts.add(config["wallets"]["from_key"])
+
+
+contract_to_mock = {
+    "eth_usd_price_feed": MockV3Aggregator,
+    "vrf_coordinator": VRFCoordinatorMock,
+    "link_token": LinkToken,
+}
+
+def get_contract(contract_name):
+    """
+    This function will grabe the contract address from the brownie
+    config if defined, it will deploy a mock version of that contract and
+    return that mock contract
+    
+        Agrs:
+           contract_name (string)
+        
+        Return:
+            brownie.network.contract.ProjectContract: The most recently deployed
+            version of this contract.
+    """
+    contract_type = contract_to_mock[contract_name]
+    if network.show_active() in LOCAL_BLOCKCHAIN_ENVIRONMENT:
+        if len(contract_type) <=0:
+            deploy_mock()
+        contract = contract_type[-1] # Grab the latest contarct eg. MockV3Aggregator[-1]
+    else:
+        contract_address = config["networks"][network.show_active()][contract_name]
+        # address
+        # ABI
+        contract = Contract.from_abi(contract_type._name, contract_address, contract_type.abi)
+    return contract
+
+def deploy_mock():
+    print(f"The active network id {network.show_active()}")
+    print("Deploying Mocks...")
+    account = get_account()
+    MockV3Aggregator.deploy(
+        DECIMALS, Web3.toWei(STARTING_PRICE, "ether"), {"from": account}
+    )  # Here 18 is decimals and 2000*10^18 is price
+    link_token = LinkToken.deploy({"from": account})
+    VRFCoordinatorMock.deploy(link_token.address, {"from": account})
+    print("Mocks Deployed!")
+
+def fund_with_link(contract_address, account=None, link_token=None, amount=100000000000000000): # 10^17 or 0.1 LINK
+    account = account if account else get_account()
+    link_token = link_token if link_token else get_contract("link_token")
+    tx = link_token.transfer(contract_address, amount, {"from": account})
+    tx.wait(1)
+    print("Fund Contract!")
+    return tx
